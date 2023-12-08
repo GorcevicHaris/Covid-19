1ba04b37b22cfcb93fde182093788c21bee4ade7
macbook <harisgorcevic1000@gmail.com>
2023-12-08 23:33:34 +0100

    Rad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  function getData() {
+    axios
+      .get(`https://corona-virus-world-and-india-data.p.rapidapi.com/api`, {
+        headers: {
+          "X-RapidAPI-Key":
+            "1b2013684fmsh5e2154cde374d29p1987b9jsnf9a0e60af14e",
+          "X-RapidAPI-Host": "corona-virus-world-and-india-data.p.rapidapi.com",
+        },
+      })
+      .then((response) => setData(response.data));
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+  return <div className="App"></div>;
 }
-
+////
 export default App;
+//
