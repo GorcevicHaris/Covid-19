4e7dc64343cbf41e58aeea72d47fa047242d9c24
macbook <harisgorcevic1000@gmail.com>
2023-12-09 20:32:05 +0100

    rad

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,6 +6,7 @@ import Card from "../Components/Card";
 function Homepage() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+
   function getData() {
     axios
       .get(`https://corona-virus-world-and-india-data.p.rapidapi.com/api`, {
@@ -25,29 +26,45 @@ function Homepage() {
   return (
     <div className="container">
       <div className="card">
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
-        <div className="div"></div>
+        <div className="div">Country</div>
+        <div className="div">Total Cases</div>
+        <div className="div">New Cases</div>
+        <div className="div">Total Deaths</div>
+        <div className="div">New Deaths</div>
+        <div className="div">Total Recovered</div>
+        <div className="div">Active Cases</div>
+        <div className="div">Serious Critical</div>
+        <div className="div">Tot Cases 1M pop</div>
+        <div className="div">Deaths/ 1M pop</div>
+        <div className="div">Total Tests</div>
+        <div className="div">Tests/ 1M pop</div>
       </div>
+
       {data.map((el, index) => (
         <div className="card">
-          <div className="div" style={{ width: "30px" }}>
-            {el.country_namem}
-            {index + 1}
-          </div>
+          <div className="div">{el.country_name}</div>
           <div className="div">{el.cases}</div>
-          <div className="div">{el.new_cases}</div>
+          <div
+            className="div"
+            style={{
+              backgroundColor: el.new_cases > 0 ? "green" : "",
+              color: "white",
+            }}
+          >
+            {el.new_cases > 0 ? <h4>+{el.new_cases}</h4> : ""}
+          </div>
           <div className="div">{el.deaths}</div>
-          <div className="div">{el.new_deaths}</div>
+          {
+            <div
+              className="div"
+              style={{
+                backgroundColor: el.new_deaths > 0 ? "red" : "",
+                color: "white",
+              }}
+            >
+              {el.new_deaths > 0 ? <h4>+{el.new_deaths}</h4> : ""}
+            </div>
+          }
           <div className="div">{el.total_recovered}</div>
           <div className="div">{el.active_cases}</div>
           <div className="div">{el.serious_critical}</div>
