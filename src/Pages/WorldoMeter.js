8adf54b819b188d495a174974713ab0c255cd7a6
macbook <harisgorcevic1000@gmail.com>
2023-12-10 22:34:35 +0100

    Rad

--- src/Pages/WorldoMeter.js
+++ src/Pages/WorldoMeter.js
@@ -10,91 +10,185 @@ export default function WorldoMeter() {
         >
           WORLD POPULATION
         </div>
-        <div className="info">8,078,116,519Current World Population</div>
-        <div className="info">126,520,453Births this year</div>
-        <div className="info">57,249,211Deaths this year</div>
-        <div className="info">69,271,386Net population growth this year</div>
+        <div className="info">8,078,116,519 &nbsp;Current World Population</div>
+        <div className="info">126,520,453 &nbsp;Births this year</div>
+        <div className="info">57,249,211 &nbsp;Deaths this year</div>
+        <div className="info">
+          69,271,386 &nbsp;Net population growth this year
+        </div>
         <div
-          style={{ fontSize: "23px", fontWeight: "100", color: "gray" }}
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
           className="info"
         >
           GOVERNMENT & ECONOMICS
         </div>
-        <div className="info">81,139,036Cars produced this year</div>
-        <div className="info">148,048,035Bicycles produced this year</div>
-        <div className="info">220,535,735Computers produced this year</div>
+        <div className="info">81,139,036 &nbsp;Cars produced this year</div>
+        <div className="info">
+          148,048,035 &nbsp;Bicycles produced this year
+        </div>
+        <div className="info">
+          220,535,735 &nbsp;Computers produced this year
+        </div>
         <div
-          style={{ fontSize: "23px", fontWeight: "100", color: "gray" }}
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
           className="info"
         >
           SOCIETY & MEDIA
         </div>
-        <div className="info">2,636,534New book titles published this year</div>
+        <div className="info">
+          2,636,534 &nbsp;New book titles published this year
+        </div>
         <div className="info">202,324,038 tv sets sold worldwide this year</div>
         <div
-          style={{ fontSize: "23px", fontWeight: "100", color: "gray" }}
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
           className="info"
         >
           ENVIRONMENT
         </div>
-        <div className="info">4,899,897Forest loss this year (hectares)</div>
+        <div className="info">
+          4,899,897 &nbsp;Forest loss this year (hectares)
+        </div>
         <div className="info">
           6,596,600Land lost to soil erosion this year (ha)
         </div>
-        <div className="info">34,476,105,303CO2 emissions this year (tons)</div>
         <div className="info">
-          11,306,348Desertification this year (hectares)
+          34,476,105,303 &nbsp;CO2 emissions this year (tons)
         </div>
         <div className="info">
-          9,226,345Toxic chemicals released in the environment this year (tons)
+          11,306,348 &nbsp;Desertification this year (hectares)
+        </div>
+        <div className="info">
+          9,226,345 &nbsp;Toxic chemicals released in the environment this year
+          (tons)
         </div>
         <div
-          style={{ fontSize: "23px", fontWeight: "100", color: "gray" }}
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
           className="info"
         >
           FOOD
         </div>
         <div className="info">
-          874,878,601Undernourished people in the world
+          874,878,601 &nbsp;Undernourished people in the world
         </div>
-        <div className="info">1,753,935,919Overweight people in the world</div>
-        <div className="info">850,162,881Obese people in the world</div>
+        <div className="info">
+          1,753,935,919 &nbsp;Overweight people in the world
+        </div>
+        <div className="info">850,162,881 &nbsp;Obese people in the world</div>
 
         <div
-          style={{ fontSize: "23px", fontWeight: "100", color: "gray" }}
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
           className="info"
         >
           WATER
         </div>
         <div className="info">
-          4,341,230,967Water used this year (million L)
+          4,341,230,967 &nbsp;Water used this year (million L)
         </div>
         <div className="info">
-          793,382Deaths caused by water related diseases this year
+          793,382 &nbsp;Deaths caused by water related diseases this year
         </div>
         <div className="info">
-          767,742,404People with no access to a safe drinking water source
+          767,742,404 &nbsp;People with no access to a safe drinking water
+          source
         </div>
 
         <div
-          style={{ fontSize: "23px", fontWeight: "100", color: "gray" }}
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
           className="info"
         >
           ENERGY
         </div>
         <div className="info">
-          366,717,845- from non-renewable sources (MWh)
+          366,717,845 &nbsp;- from non-renewable sources (MWh)
         </div>
-        <div className="info">64,880,513- from renewable sources (MWh)</div>
         <div className="info">
-          2,699,899,222,857Solar energy striking Earth today (MWh){" "}
+          64,880,513 &nbsp;- from renewable sources (MWh)
         </div>
-        <div className="info">1,383,698,091,624Oil left (barrels) </div>
-        <div className="info">14,430Days to the end of oil (~40 years)</div>
-        <div className="info">1,071,132,447,923Natural Gas left (boe)</div>
-        <div className="info">56,375Days to the end of natural gas</div>
-        <div className="info">4,278,747,730,497Coal left (boe)</div>
-        <div className="info">147,543Days to the end of coa</div>
+
+        <div className="info">1,383,698,091,624 &nbsp;Oil left (barrels) </div>
+        <div className="info">
+          14,430 &nbsp;Days to the end of oil (~40 years)
+        </div>
+        <div className="info">
+          1,071,132,447,923 &nbsp;Natural Gas left (boe)
+        </div>
+        <div className="info">56,375 &nbsp;Days to the end of natural gas</div>
+        <div className="info">4,278,747,730,497 &nbsp;Coal left (boe)</div>
+        <div className="info">147,543 &nbsp;Days to the end of coa</div>
+        <div
+          style={{
+            fontSize: "23px",
+            fontWeight: "100",
+            color: "gray",
+            marginTop: "20px",
+          }}
+          className="info"
+        >
+          HEALTH
+        </div>
+        <div className="info">
+          12,230,835 &nbsp;Communicable disease deaths this year
+        </div>
+        <div className="info">476,313 &nbsp;Seasonal flu deaths this year</div>
+        <div className="info">
+          7,161,516 &nbsp;Deaths of children under 5 this year
+        </div>
+        <div className="info">42,149,372 &nbsp;Abortions this year </div>
+        <div className="info">
+          291,215 &nbsp;Deaths of mothers during birth this year
+        </div>
+        <div className="info">
+          1,583,857 &nbsp;Deaths caused by HIV/AIDS this year
+        </div>
+        <div className="info">
+          7,738,021 &nbsp;Deaths caused by cancer this year
+        </div>
+        <div className="info">
+          371,538 &nbsp;Deaths caused by malaria this year
+        </div>
+        <div className="info">
+          4,709,975 &nbsp;Deaths caused by smoking this year
+        </div>
+        <div className="info">
+          2,356,475 &nbsp;Deaths caused by alcohol this year
+        </div>
+        <div className="info">1,010,343 &nbsp;Suicides this year</div>
+        <div className="info">
+          $ 376,917,580,909 &nbsp;Money spent on illegal drugs this year
+        </div>
+        <div className="info">
+          1,271,845 &nbsp;Road traffic accident fatalities this year
+        </div>
       </div>
     </div>
   );
