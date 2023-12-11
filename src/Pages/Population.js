c0fac4f8beb156f35de4c48b2e648fb165b9ebda
macbook <harisgorcevic1000@gmail.com>
2023-12-11 15:02:40 +0100

    rad

--- src/Pages/Population.js
+++ src/Pages/Population.js
@@ -4,11 +4,12 @@ import "./population.css";
 function Population() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+  const [falsing, setFalsing] = useState(false);
   const api_key = "P131FrYx2KTYpdn2gMRjpw==t9WuATR52yhdG9X3";
 
   function getData() {
     axios
-      .get(`https://api.api-ninjas.com/v1/country?name=united sta`, {
+      .get(`https://api.api-ninjas.com/v1/country?name=${search}`, {
         headers: { "X-Api-Key": `${api_key}` },
         contentType: "application/json",
       })
@@ -18,9 +19,22 @@ function Population() {
   }
   useEffect(() => {
     getData();
-  }, []);
+  }, [search]);
+  console.log(search);
   console.log(data);
-  return <div className="population"></div>;
+  return (
+    <div className="population">
+      <div className="inpbtn">
+        <input
+          className="inp1"
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+        <button onClick={() => setFalsing(true)}>Search</button>
+      </div>
+
+      {falsing && data.map((el) => <div>{el.population}</div>)}
+    </div>
+  );
 }
 
 export default Population;
